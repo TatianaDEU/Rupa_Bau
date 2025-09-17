819a45eab65b6d9e509ad644610e95027d1a28b6
Tatiana Miller <tatiana.miller@proplant-mvi.com>
2025-09-17 12:05:09 +0200

    All

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,7 +1,17 @@
+import React, { useState } from "react";
 import { Image } from "./image";
-import React from "react";
 
-export const Gallery = (props) => {
+export const Gallery = ({ data }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (largeImage) => {
+    setSelectedImage(largeImage);
+  };
+
+  const handleClose = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div id="portfolio" className="text-center">
       <div className="container">
@@ -11,18 +21,17 @@ export const Gallery = (props) => {
             Werfen Sie einen Blick auf unsere abgeschlossenen Bauprojekte und lassen Sie sich von Qualität, Design und Präzision inspirieren.
           </p>
         </div>
+
         <div className="row">
           <div className="portfolio-items">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div
-                    key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4"
-                  >
+            {data
+              ? data.map((d, i) => (
+                  <div key={`${d.title}-${i}`} className="col-sm-6 col-md-4 col-lg-4">
                     <Image
                       title={d.title}
-                      largeImage={d.largeImage}
                       smallImage={d.smallImage}
+                      largeImage={d.largeImage}
+                      onClick={handleImageClick}
                     />
                   </div>
                 ))
@@ -30,6 +39,32 @@ export const Gallery = (props) => {
           </div>
         </div>
       </div>
+
+      {/* Modal für das große Bild */}
+      {selectedImage && (
+        <div
+          onClick={handleClose}
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.8)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 1000,
+            cursor: "pointer",
+          }}
+        >
+          <img
+            src={selectedImage}
+            alt="Enlarged"
+            style={{ maxHeight: "90%", maxWidth: "90%" }}
+          />
+        </div>
+      )}
     </div>
   );
 };
