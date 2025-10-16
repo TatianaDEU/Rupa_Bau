3973bede3b620bee5a6fc6ea2ca9279e25b55e57
Tatiana Miller <tatianamiller@Mac.fritz.box>
2025-10-16 12:46:32 +0200

    Animation für Services

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // AOS CSS
 
 export const Services = (props) => {
+  useEffect(() => {
+    AOS.init({ 
+      duration: 800,   // Dauer der Animation
+      easing: "ease-in-out",
+      once: true       // Animation nur einmal beim Scrollen
+    });
+  }, []);
+
   return (
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
           <h2>Unsere Leistungen</h2>
-          <p>
-            Individuelle Lösungen für jedes Bauvorhaben.
-          </p>
+          <p>Individuelle Lösungen für jedes Bauvorhaben.</p>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
+                <div 
+                  key={`${d.name}-${i}`} 
+                  className="col-md-4"
+                  data-aos="fade-up"           // Animation beim Scrollen
+                  data-aos-delay={i * 100}    // kleine Verzögerung für Stagger-Effekt
+                >
                   <i className={d.icon}></i>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
